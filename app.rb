b64b3661ee847a09f9c32b902f3a5c5a76aa0696
Sara Gorecki <sgorecki@gmail.com>
2013-12-10 16:16:45 -0500

    add root directory to page

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
+require "bundler"
 require_relative "./lib/artist.rb"
 require_relative "./lib/genre.rb"
 require_relative "./lib/song.rb"
 require_relative "./lib/parser.rb"
 require_relative "./lib/playlister.rb"
+Bundler.require
 
-playlist = Playlister.new(Parser.new("data").song_list)
-playlist.playlister
+module Playlist
+  class PlaylisterApp < Sinatra::Application
+
+    before do
+      @playlist = Playlister.new(Parser.new("data").song_list)
+    end
+
+    get "/" do
+      erb :select
+    end
+
+  end
+end
